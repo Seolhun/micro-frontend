1645cbd0627af35170d2575261b8e32759da1393
Seolhun <shun10114@gmail.com>
2019-12-20 02:26:05 +0900

    Add build scripts and fix lint

--- home/pages/api/notices/index.ts
+++ home/pages/api/notices/index.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
  * @see /api/notices
  * @returns Musicians[]
  */
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default (_req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Content-Type', 'application/json');
   res.statusCode = 200;
   res.end(JSON.stringify({ name: '/api/notices' }));
