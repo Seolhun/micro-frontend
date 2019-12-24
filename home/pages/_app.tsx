3fdd88d075477e09d7c54911ccead76618e1efdd
Seolhun <shun10114@gmail.com>
2019-12-24 15:02:17 +0900

    Fix _app props

--- home/pages/_app.tsx
+++ home/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AppContextType } from 'next/dist/next-server/lib/utils';
 
 import { appWithTranslation } from '@src/i18n';
 
@@ -9,6 +10,14 @@ interface HiCordAppProps {
 
 const HiCordApp = ({ Component, pageProps }: HiCordAppProps) => {
   return <Component {...pageProps} />;
+};
+
+HiCordApp.getInitialProps = async ({ Component, ctx }: AppContextType) => {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  return { pageProps };
 };
 
 export default appWithTranslation(HiCordApp);
